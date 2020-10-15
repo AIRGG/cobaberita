a69c211eb8147d8b84b8bb56eaace7d68b03888d
Airlangga Yudiatama <43308693+AIRGG@users.noreply.github.com>
2020-10-15 12:42:38 +0700

    Add files via upload

---
+++ listberita.js
@@ -0,0 +1,7 @@
+let berita = [
+	{
+		judul:'ini judul',
+		isi:`<h1 style="text-align: center;">Ini Adalah Judul</h1><p>&nbsp; &nbsp; ini adalah paragraf bla bla bla</p><p>asjdhjashdjas:</p><p><ul><li>aaaa</li><li>bbbb</li><li>cccc</li></ul></p>`,
+		img:'ini gbr',	
+	},
+]
